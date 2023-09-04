e266ba04422bada9468771517f72c8ccef6929ce
frimac <frimac@saludpol.gob.pe>
2023-09-04 17:29:32 -0500

    create tabla persona y persona detalle

--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -2,10 +2,81 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Persona extends Model
 {
-    use HasFactory;
+    protected $fillable = ['nro_brevete', 'codigo', 'tipo_documento', 'numero_documento', 'nombres', 'apellido_paterno', 'apellido_materno', 'fecha_nacimiento', 'sexo', 'telefono', 'email', 'foto', 'ocupacion', 'titular_id', 'tipo_relacion'];
+
+    // contantes SEXO
+    const SEXO_FEMENINO = 'F';
+    const SEXO_MASCULINO = 'M';
+    // contantes TIPO DOCUMENTO
+    const TIPO_DOCUMENTO_DNI = 'DNI';
+    const TIPO_DOCUMENTO_CARNET_EXTRANJERIA = 'CARNET_EXTRANJERIA';
+    const TIPO_DOCUMENTO_PASAPORTE = 'PASAPORTE';
+    const TIPO_DOCUMENTO_RUC = 'RUC';
+    const TIPO_DOCUMENTO_CEDULA = 'CEDULA';
+    const TIPO_DOCUMENTO_PTP = 'PTP/PTEP';
+    const TIPO_DOCUMENTO_CPP = 'CPP/CSR';
+
+    public function afiliaciones()
+    {
+        return $this->hasMany(Afiliacion::class);
+    }
+
+
+    function getPersonas($empresa_id){
+      //  $ubicacion = UbicacionTrabajo::where("ubicacion_empresa_id", $empresa_id)->first();
+       // $afiliaciones = Afiliacion::where("ubicacion_id", $ubicacion->id)->get("persona_id");
+        //$data = Persona::find($afiliaciones);
+      
+       // return $data;
+    }
+
+    function getPersona($tipo_documento,$numero_documento){
+
+        $cad = "select t1.*
+		from personas t1
+		Where t1.tipo_documento='".$tipo_documento."' And t1.numero_documento='".$numero_documento."'";
+		//echo $cad;
+		$data = DB::select($cad);
+        if(isset($data[0]))return $data[0];
+
+    }
+
+	public function listar_persona_ajax($p){
+		return $this->readFunctionPostgres('sp_listar_persona_paginado',$p);
+    }
+
+	function getPersonaBuscar($term){
+
+        $cad = "select id,nombres||' '||apellido_paterno||' '||apellido_materno persona
+		from personas
+		where estado='1'
+		and nombres||' '||apellido_paterno||' '||apellido_materno ilike '%".$term."%' ";
+
+		$data = DB::select($cad);
+        return $data;
+    }
+	
+
+	public function readFunctionPostgres($function, $parameters = null){
+
+      $_parameters = '';
+      if (count($parameters) > 0) {
+          $_parameters = implode("','", $parameters);
+          $_parameters = "'" . $_parameters . "',";
+      }
+	  DB::select("BEGIN;");
+	  $cad = "select " . $function . "(" . $_parameters . "'ref_cursor');";
+	  DB::select($cad);
+	  $cad = "FETCH ALL IN ref_cursor;";
+	  $data = DB::select($cad);
+	  DB::select("END;");
+      return $data;
+   }
+
 }
