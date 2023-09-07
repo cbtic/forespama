4eeee0dd68caf71dcdb97cc16fe7edc29628514f
Rafael Medina <rmedina@saludpol.gob.pe>
2023-09-07 15:14:24 -0500

    Creando formulario y su componente con php artisan make:model-form {model} y php artisan make:component Forms/Conductores y Usando la libreria https://github.com/GrafiteInc/Forms: https://docs.grafite.ca/utilities/forms

--- resources/views/conductores/create.blade.php
+++ resources/views/conductores/create.blade.php
@@ -8,31 +8,7 @@
                     <div class="card-header">Create Conductores</div>
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route('conductores.store') }}">
-                            @csrf
-
-                            <div class="form-group row">
-                                <label for="title" class="col-sm-4 col-form-label text-md-right">Title</label>
-
-                                <div class="col-md-6">
-                                    <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title" value="{{ old('title') }}" required autofocus>
-
-                                    @if ($errors->has('title'))
-                                        <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('title') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Create
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                        <x-forms.conductores></x-forms.conductores>
                     </div>
                 </div>
             </div>
