c3c1eb7948478b9f0b44e340b162626266dbf92f
cbtic <cbtic>
2024-01-04 08:08:12 -0500

    Act. integracion 04012024

--- app/Models/TablaMaestra.php
+++ app/Models/TablaMaestra.php
@@ -2,70 +2,6 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-//use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use DB;
-class TablaMaestra extends Model
-{
-	function getCajaAllTipo($tipo){
-
-        $cad = "select t1.id,t1.denominacion,t1.codigo 
-		from tabla_maestras t1
-		where t1.tipo='".$tipo."' and t1.estado='A' 
-		And t1.id not in (select distinct id_caja from caja_ingresos where estado = '1')
-		order by t1.id ";
-    
-		$data = DB::select($cad);
-        return $data;
-    }
-	
-    function getMaestro($tipo, $codigo){
-
-        $cad = "select id,denominacion 
-                from tabla_maestras 
-                where tipo='".$tipo."' 
-                and codigo like '".$codigo."%'
-                order by orden ";
-    
-		$data = DB::select($cad);
-        return $data;
-    }
-	
-	function getMaestroByTipo($tipo){
-
-        $cad = "select id,denominacion 
-                from tabla_maestras 
-                where tipo='".$tipo."' 
-                order by orden ";
-    
-		$data = DB::select($cad);
-        return $data;
-    }
-	
-	function getMaestroByTipoAndTipoNombre($tipo,$tipo_nombre){
-
-        $cad = "select id,denominacion 
-                from tabla_maestras 
-                where tipo='".$tipo."' 
-				And tipo_nombre='".$tipo_nombre."' 
-                order by orden ";
-    
-		$data = DB::select($cad);
-        return $data;
-    }
-	
-	function getMaestroByTipoIn($tipo1,$tipo2){
-
-        $cad = "select id,denominacion 
-                from tabla_maestras 
-                where tipo in('".$tipo1."','".$tipo2."') 
-                order by 1 asc ";
-    
-		$data = DB::select($cad);
-        return $data;
-    }
-=======
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use DB;
@@ -117,5 +53,4 @@ class TablaMaestra extends Model
         $data = DB::select($cad);
         return $data;
      }
->>>>>>> 9a4f1f2bb159b571d65fee9572af8b1a5a93ac9f
 }
--- database/migrations/2023_09_02_053455_create_conductores_table.php
+++ database/migrations/2023_09_02_053455_create_conductores_table.php
@@ -14,16 +14,9 @@ class CreateConductoresTable extends Migration
     public function up()
     {
         Schema::create('conductores', function (Blueprint $table) {
-<<<<<<< HEAD
-            $table->bigIncrements('id');
-            $table->string('licencia');
-            $table->bigInteger('id_personas')->unsigned()->index();
-            $table->string('estado',1)->default('1');
-=======
             $table->id();
             $table->bigInteger('personas_id')->unsigned()->index();
             $table->enum('estado', ['ACTIVO', 'CANCELADO']);
->>>>>>> 9a4f1f2bb159b571d65fee9572af8b1a5a93ac9f
             $table->timestamps();
             //Foreign Keys
             $table->foreign('id_personas')->references('id')->on('personas');
