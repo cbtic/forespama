86134495cc18cf3e7701e34a7881b789b1ee27a2
Rafael Medina <rmedina@saludpol.gob.pe>
2024-06-03 16:27:05 -0500

    Cambiando el x-form de tablamaestra a TablaMaestra

--- resources/views/frontend/tablamaestras/show.blade.php
+++ resources/views/frontend/tablamaestras/show.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">Ver Tabla #{{ $tablamaestras->id }}</div>
 
                 <div class="card-body">
-                    <x-forms.tablamaestra :tablamaestras="$tablamaestras"></x-forms.tablamaestra>
+                    <x-forms.TablaMaestra :tablamaestras="$tablamaestras" />
                 </div>
             </div>
         </div>
