0e039c275e322383e892cbcab4ccbf5efa44e997
Rafael Medina Palacios <rafaelmedinapalacios@Rafaels-MacBook-Pro.local>
2024-06-23 22:18:00 -0500

    Corregido el javascript para los Select2 en los popups de Nuevos y Editar Productos en el Detalle de Entrada y Salida respectivamente. Tanto en Create y Edit para Entradas y Salidas de productos.

--- resources/views/frontend/entrada_productos/edit.blade.php
+++ resources/views/frontend/entrada_productos/edit.blade.php
@@ -39,42 +39,48 @@
         }, 500);
     }
 
-    $('#Id_moneda').select2().on('change', function(e) {
-        var x = this.value;
-        if (x==1) {
-                $("#Tipo_cambio_dolar").hide();
-                $("#Tipo_cambio_dolar").val(0);
-            } else {
-                $("#Tipo_cambio_dolar").show();
-                $("#Tipo_cambio_dolar").val(3.85);
-            }
-    });
-
-    $('#Igv_compra').select2().on('change', function(e) {
-        var x = this.value;
-        if (x==0) {
-            $("#Total_compra").val($("#Sub_total_compra").val());
-        } else {
-            $("#Total_compra").val($("#Sub_total_compra").val()*1.18);
-        }
-    });
-
-    jQuery(document).ready(function() {
+    $(document).ready(function() {
         $('.form-select').select2();
         $('.form-select').select2({dropdownAutoWidth : true});
         if($('#Id_moneda').val()==1) {
             $('#Tipo_cambio_dolar').hide();
             $('#Tipo_cambio_dolar').val(0);
         }
+
+        $('#Id_moneda').select2().on('change', function(e) {
+            var x = this.value;
+            if (x==1) {
+                    $("#Tipo_cambio_dolar").hide();
+                    $("#Tipo_cambio_dolar").val(0);
+                } else {
+                    $("#Tipo_cambio_dolar").show();
+                    $("#Tipo_cambio_dolar").val(3.85);
+                }
+        });
+
+        $('#Igv_compra').select2().on('change', function(e) {
+            var x = this.value;
+            if (x==0) {
+                $("#Total_compra").val($("#Sub_total_compra").val());
+            } else {
+                $("#Total_compra").val($("#Sub_total_compra").val()*1.18);
+            }
+        });
+
+        $('.btn.btn-success').on('click', function() {
+            setTimeout(function(){
+                // alert(500);
+                $('.form-select').select2();
+            }, 500)
+        });
+
+        $('.btn.btn-default').on('click', function() {
+            setTimeout(function(){
+                // alert(500);
+                $('.form-select').select2();
+            }, 500)
+        });
     });
-
-    $(".btn.btn-success").click(setTimeout(function(){
-        redimensionaSelect2();
-    }, 500));
-
-    $(".btn.btn-default").click(setTimeout(function(){
-        redimensionaSelect2();
-    }, 500));
 
     function redimensionaSelect2(){
         $('.form-select').select2({dropdownAutoWidth : true});
