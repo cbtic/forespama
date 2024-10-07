8642eeba153588ad0fceec94eed13b4dee8dfad3
Freddy <frimacc@gmail.com>
2024-10-06 23:39:24 -0500

    AddIdUsuarioRecibeToEntradaProductosTable y salida

--- database/migrations/2024_10_07_043809_add_id_usuario_recibe_to_salida_productos_table.php
+++ database/migrations/2024_10_07_043809_add_id_usuario_recibe_to_salida_productos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIdUsuarioRecibeToSalidaProductoDetallesTable extends Migration
+class AddIdUsuarioRecibeToSalidaProductosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddIdUsuarioRecibeToSalidaProductoDetallesTable extends Migration
      */
     public function up()
     {
-        Schema::table('salida_producto_detalles', function (Blueprint $table) {
+        Schema::table('salida_productos', function (Blueprint $table) {
             $table->bigInteger('id_usuario_recibe')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class AddIdUsuarioRecibeToSalidaProductoDetallesTable extends Migration
      */
     public function down()
     {
-        Schema::table('salida_producto_detalles', function (Blueprint $table) {
+        Schema::table('salida_productos', function (Blueprint $table) {
             //
         });
     }
