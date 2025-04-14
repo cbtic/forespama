6da5a3bb147f63a5992f04b96eee6ad4b9a3e734
Cesar <julioyamunaque04@gmail.com>
2025-04-14 16:35:33 -0500

    act sp factura sodimac

--- database/procedures/sp_listar_sodimac_factura_pago_paginado.sql
+++ database/procedures/sp_listar_sodimac_factura_pago_paginado.sql
@@ -12,6 +12,7 @@ v_tabla varchar;
 v_where varchar;
 v_count varchar;
 v_col_count varchar;
+v_filtros_fecha varchar;
 
 begin
 	
@@ -19,11 +20,21 @@ begin
 
 	v_campos=' id, serie, numero, tipo, fecha, destinatario, cod_tributario, subtotal, impuesto, total, estado_pago, anulado, sunat, numero_orden_compra_cliente, moneda, numero_documento_sodimac, importe_total ';
 
+	v_filtros_fecha := '';
+	
+	IF p_fecha_ini <> '' THEN
+	  v_filtros_fecha := v_filtros_fecha || ' AND c.fecha >= TO_DATE(''' || p_fecha_ini || ''', ''dd-mm-yyyy'') ';
+	END IF;
+	
+	IF p_fecha_fin <> '' THEN
+	  v_filtros_fecha := v_filtros_fecha || ' AND c.fecha <= TO_DATE(''' || p_fecha_fin || ''', ''dd-mm-yyyy'') ';
+	END IF;
+
 	v_tabla=' (select c.id, c.serie, c.numero, c.tipo, TO_CHAR(c.fecha,''dd-mm-yyyy'') fecha, c.destinatario, c.cod_tributario, c.subtotal, c.impuesto, c.total, c.estado_pago, c.anulado, c.estado_sunat sunat, (select oc.numero_orden_compra_cliente from orden_compras oc inner join orden_compra_detalles ocd on oc.id = ocd.id_orden_compra left join valorizaciones v on ocd.id = v.pk_registro where v.id_comprobante = c.id and oc.estado =''1'' limit 1) numero_orden_compra_cliente, c.moneda, sfd.numero_documento numero_documento_sodimac, sfd.importe_total  ' ||
               'FROM comprobantes c ' ||
               'left join sodimac_factura_detalles sfd on ''01-'' || c.serie ||''-''|| lpad(coalesce(c.numero::int, 1)::varchar, 8, ''0'') = sfd.numero_documento  '||
-              'Where 1 = 1 and  c.id_empresa in (''23'',''187'') '||
-              'UNION ALL ' ||
+              'Where 1 = 1 and  c.id_empresa in (''23'',''187'') '|| v_filtros_fecha || '
+               UNION ALL ' ||
               'select csh.id, csh.serie, csh.numero, csh.tipo, TO_CHAR(csh.fecha,''dd-mm-yyyy'') fecha, csh.destinatario, csh.cod_tributario, csh.subtotal, csh.impuesto, csh.total, '''' estado_pago, '''' anulado, '''' sunat, '''' numero_orden_compra_cliente, csh.moneda, sfd2.numero_documento, sfd2.importe_total ' ||
               'from comprobante_sodimac_historicos csh ' ||
               'left join sodimac_factura_detalles sfd2 on ''01-'' || csh.serie ||''-''|| lpad(coalesce(csh.numero::int, 1)::varchar, 8, ''0'') = sfd2.numero_documento ) union_table ';
@@ -33,14 +44,6 @@ begin
 	/*If p_denominacion<>'' Then
 	 v_where:=v_where||'And ep.denominacion ilike  ''%'||p_denominacion||'%'' ';
 	End If;*/
-
-	If p_fecha_ini<>'' Then
-	 v_where:=v_where||'And fecha >= '''||p_fecha_ini||''' ';
-	End If;
-
-	If p_fecha_fin<>'' Then
-	 v_where:=v_where||'And fecha <= '''||p_fecha_fin||''' ';
-	End If;
 
 	If p_tipo<>'' Then
 	 v_where:=v_where||'And tipo = '''||p_tipo||''' ';
