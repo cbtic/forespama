bd1cad18d65e74906e7d12e0e596048a90633bb1
Cesar <julioyamunaque04@gmail.com>
2025-07-02 11:10:46 -0500

    act pdf de dispensacion

--- app/Models/DispensacionDetalle.php
+++ app/Models/DispensacionDetalle.php
@@ -15,9 +15,9 @@ class DispensacionDetalle extends Model
         $cad = "select dd.id, ROW_NUMBER() OVER (PARTITION BY dd.id_dispensacion) AS row_num, p.numero_serie, p.denominacion producto, p.codigo, dd.cantidad, tm.denominacion estado_producto, tm2.denominacion unidad_medida, m.denominiacion marca from dispensacion_detalles dd 
         inner join dispensaciones d on dd.id_dispensacion = d.id 
         inner join productos p on dd.id_producto = p.id 
-        inner join tabla_maestras tm on dd.id_estado_producto ::int = tm.codigo::int and tm.tipo = '56'
-        inner join tabla_maestras tm2 on dd.id_estado_producto ::int = tm2.codigo::int and tm2.tipo = '43'
-        inner join marcas m on dd.id_marca = m.id 
+        left join tabla_maestras tm on dd.id_estado_producto ::int = tm.codigo::int and tm.tipo = '56'
+        left join tabla_maestras tm2 on dd.id_unidad_medida ::int = tm2.codigo::int and tm2.tipo = '43'
+        left join marcas m on dd.id_marca = m.id 
         where d.id ='".$id."'";
 
 		$data = DB::select($cad);
