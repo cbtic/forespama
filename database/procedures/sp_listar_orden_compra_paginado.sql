1ddaffe881e1d616e70349fb16c83cb613b46991
Cesar <julioyamunaque04@gmail.com>
2025-09-22 18:03:32 -0500

    act sp de autorizacion

--- database/procedures/sp_listar_orden_compra_paginado.sql
+++ database/procedures/sp_listar_orden_compra_paginado.sql
@@ -1,4 +1,4 @@
--- DROP FUNCTION public.sp_listar_orden_compra_paginado(varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, refcursor);
+-- DROP FUNCTION public.sp_listar_orden_compra_paginado(varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, refcursor);
 
 CREATE OR REPLACE FUNCTION public.sp_listar_orden_compra_paginado(p_tipo_documento character varying, p_empresa_compra character varying, p_empresa_vende character varying, p_fecha_inicio character varying, p_fecha_fin character varying, p_numero_orden_compra character varying, p_numero_orden_compra_cliente character varying, p_situacion character varying, p_almacen_origen character varying, p_almacen_destino character varying, p_estado character varying, p_id_user character varying, p_id_vendedor character varying, p_estado_pedido character varying, p_prioridad character varying, p_canal character varying, p_tipo_producto character varying, p_pagina character varying, p_limit character varying, p_ref refcursor)
  RETURNS refcursor
@@ -35,7 +35,7 @@ begin
 	from orden_compra_contacto_entregas occe 
 	where occe.id_orden_compra = oc.id) then 1 else 0 
 	end) tiene_direccion, oc.total, oc.estado_pedido,
-	(select r.codigo  from requerimientos r where oc.id_requerimiento = r.id) codigo_requerimiento ';
+	(select r.codigo  from requerimientos r where oc.id_requerimiento = r.id) codigo_requerimiento, oc.id_autorizacion ';
 
 	v_tabla=' from orden_compras oc 
 	inner join empresas e2 on oc.id_empresa_vende = e2.id
